ee7fff1b2f6a91126df8bbd94157c06777cef60e
BillXiangXBB <billxiangxbb@163.com>
2019-07-14 12:09:48 +0800

    项目初始化

---
+++ common/src/main/java/com/xbb/javakv/common/utils/LogUtil.java
@@ -0,0 +1,35 @@
+package com.xbb.javakv.common.utils;
+
+/**
+ * 日志打印工具类
+ *
+ * @author BillXiang
+ * @version 1.0.0
+ */
+public class LogUtil {
+
+    /**
+     * 打印异常堆栈
+     *
+     * @param e 异常
+     * @return 堆栈
+     */
+    public static String exceptionStack(Throwable e) {
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append("Occur Throwable: ").append(e.getClass().getName())
+                .append(": ").append(e.getMessage()).append("\n");
+        exceptionStackTraces(e, strBuilder);
+        return strBuilder.toString();
+    }
+
+    private static void exceptionStackTraces(Throwable e, StringBuilder strBuilder) {
+        for (StackTraceElement element: e.getStackTrace()) {
+            strBuilder.append("\t").append(element.toString()).append("\n");
+        }
+        if (e.getCause() != null) {
+            strBuilder.append("Caused by:").append("\n");
+            exceptionStackTraces(e.getCause(), strBuilder);
+        }
+    }
+
+}
